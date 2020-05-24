ca13df10b07793101a4d986c29f5eb442afc5404
Zakaria Talhami <zakaria.a.a.talhami@gmail.com>
2020-05-24 04:37:05 +0300

    feat: Add Auth templates

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,7 +5,7 @@ import os
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_kEY="dev",
+        SECRET_KEY="dev",
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
